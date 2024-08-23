8a55144aff21c38942be9d5832a436d21a11896b
unknown <vannchhit0709@gmail.com>
2024-08-23 16:44:28 +0700

    feature signup working

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// Functional component with JSX returned properly
+const App = () => {
+  return (
+    <div>
+      <h1>Chhit Chhit</h1>
+      <p>I am chit</p>
+    </div>
+  );
+};
+
+export default App;
